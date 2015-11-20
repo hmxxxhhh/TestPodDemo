1ae36e9ee6f8c882b166abc0fe8a5900b72777af
humingxing <humingxing@mitake.com.cn>
2015-11-20 13:46:32 +0800

    first

---
+++ TestPod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPod
+//
+//  Created by IOS_HMX on 15/11/20.
+//  Copyright © 2015年 humingxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestPod/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestPod
+//
+//  Created by IOS_HMX on 15/11/20.
+//  Copyright © 2015年 humingxing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
